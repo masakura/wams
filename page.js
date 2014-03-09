0eb2c0a3980b6f79df2087edc7d81bcc0bf31b32
Tomo Masakura <masakura@codearts.co.jp>
2014-03-09 22:14:17 +0900

    後々の修正をわかりやすくするためのリファクタリング

--- page.js
+++ page.js
@@ -8,9 +8,7 @@ $(function() {
     // Read current data and rebuild UI.
     // If you plan to generate complex UIs like this, consider using a JavaScript templating library.
     refreshTodoItems = function() {
-        var query = todoItemTable.where({ complete: false });
-
-        query.read().then(function(todoItems) {
+        var refresh = function(todoItems) {
             var listItems = $.map(todoItems, function(item) {
                 return $('<li>')
                     .attr('data-todoitem-id', item.id)
@@ -21,7 +19,10 @@ $(function() {
 
             $('#todo-items').empty().append(listItems).toggle(listItems.length > 0);
             $('#summary').html('<strong>' + todoItems.length + '</strong> item(s)');
-        }, handleError);
+        };
+
+        todoItemTable.where({ complete: false }).read()
+            .then(refresh, handleError);
     }
 
     function handleError(error) {
@@ -38,7 +39,8 @@ $(function() {
         var textbox = $('#new-item-text'),
             itemText = textbox.val();
         if (itemText !== '') {
-            todoItemTable.insert({ text: itemText, complete: false }).then(refreshTodoItems, handleError);
+            todoItemTable.insert({ text: itemText, complete: false })
+                .then(refreshTodoItems, handleError);
         }
         textbox.val('').focus();
         evt.preventDefault();
@@ -47,17 +49,20 @@ $(function() {
     // Handle update
     $(document.body).on('change', '.item-text', function() {
         var newText = $(this).val();
-        todoItemTable.update({ id: getTodoItemId(this), text: newText }).then(null, handleError);
+        todoItemTable.update({ id: getTodoItemId(this), text: newText })
+            .then(null, handleError);
     });
 
     $(document.body).on('change', '.item-complete', function() {
         var isComplete = $(this).prop('checked');
-        todoItemTable.update({ id: getTodoItemId(this), complete: isComplete }).then(refreshTodoItems, handleError);
+        todoItemTable.update({ id: getTodoItemId(this), complete: isComplete })
+            .then(refreshTodoItems, handleError);
     });
 
     // Handle delete
     $(document.body).on('click', '.item-delete', function () {
-        todoItemTable.del({ id: getTodoItemId(this) }).then(refreshTodoItems, handleError);
+        todoItemTable.del({ id: getTodoItemId(this) })
+            .then(refreshTodoItems, handleError);
     });
 });
 
